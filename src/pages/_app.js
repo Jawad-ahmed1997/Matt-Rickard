34a5c09d96d44bcc40416f79f6251a19f4d4ee8f
jawad-ahmed1997 <jawad.walbi@gmail.com>
2024-05-29 20:22:05 +0500

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,13 @@
 'use client'
 import DynamicLayout from "@/components/Layout";
+import { Montserrat  } from 'next/font/google'
 
 import "@/styles/globals.css";
 
+const montserrat  = Montserrat ({ subsets: ['latin'] })
 export default function App({ Component, pageProps }) {
   return (
-    <DynamicLayout>
+    <DynamicLayout  className={montserrat.className}>
       <Component {...pageProps} />
     </DynamicLayout>
   )
